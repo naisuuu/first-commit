9f552d292f9e6a4035dc499b9e2fffd4873025eb
Cyril Jacob Mosawan <x19101546@student.ncirl.ie>
2019-10-10 13:15:13 +0100

    first commit

---
+++ asda.js
@@ -0,0 +1,8 @@
+var tt = prompt("Which times tables would you like to view?");
+
+document.write("Here are the "+tt+"times tables:<br/>");
+for(var i = 1; i<=Answer
+;i++){
+
+     document.write(i+" times "+tt+"= "+(i*tt));
+}
